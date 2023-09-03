80c2eea36d714696ab973631b42ca6bd89cbfd11
ochosteve <ochosteve08@gmail.com>
2023-09-04 00:26:46 +0100

    add slider to home page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,35 +1,54 @@
 import { Form } from "react-router-dom";
-import {FaUserPlus} from 'react-icons/fa'
-
+import { FaUserPlus } from "react-icons/fa";
+import Slider from "./Slider";
 
 const Landing = () => {
   return (
-    <div className="intro">
-      <div>
-        <h1>
-          Take Control of <span className="accent">Your Money</span>
-        </h1>
-        <p>
-          {" "}
-          Personal budgeting is the secret to financial freedom. Start your
-          journey today.
+    <div>
+      <div
+        style={{
+          marginBottom: "30px",
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%", // Adjust the height value accordingly if you have a specific height in mind
+        }}
+      >
+        <h1 style={{ color: "#2d1387" }}>Budget with purpose</h1>
+        <p style={{ padding: "20px" }}>
+          Spend, save, and give toward what&apos;s important in life
         </p>
-        <Form method="post">
-          <input
-            type="text"
-            placeholder="username"
-            name="username"
-            required
-            aria-label="your name"
-            autoComplete="given-name"
-          />
-          <button type="submit" className="btn btn--dark">
-            Create Account <FaUserPlus />
-          </button>
-        </Form>
+      </div>
+
+      <div className="intro">
+        <div>
+          <h2>
+            Take Control of <span className="accent">Your Money</span>
+          </h2>
+          <p>
+            Personal budgeting is the secret to financial freedom. Start your
+            journey today.
+          </p>
+          <Form method="post">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              aria-label="your name"
+              autoComplete="given-name"
+            />
+            <button type="submit" className="btn btn--dark">
+              Create Account <FaUserPlus />
+            </button>
+          </Form>
+        </div>
+        <Slider />
       </div>
     </div>
   );
-}
+};
 
-export default Landing
+export default Landing;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,32 @@
-
+import { useEffect, useState } from "react";
+import image1 from "../assets/landing1.svg";
+import image2 from "../assets/loan2.png";
+import image3 from "../assets/loan6.png";
+import image4 from "../assets/finance.svg"
+import image5 from "../assets/loan7.svg"
 
 const Slider = () => {
-  return (
-    <div>Slider</div>
-  )
-}
+  const images = [image1, image2, image3,image4, image5];
+   const [currentIndex, setCurrentIndex] = useState(0);
 
-export default Slider
+  useEffect(() => {
+    const next = (currentIndex + 1) % images.length;
+    const id = setTimeout(() => setCurrentIndex(next), 4000);
+    return () => clearTimeout(id);
+  }, [currentIndex, images.length]);
+  return (
+    <div className="carousel">
+      {images.map((img, index) => (
+        <div
+          key={index}
+          className={`carousel__image ${
+            index === currentIndex ? "active" : ""
+          }`}
+          style={{ backgroundImage: `url(${img})` }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
