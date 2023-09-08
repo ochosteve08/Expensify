0288f0c0cbb08672cde1f02f0e7619f56e2ec82d
ochosteve <ochosteve08@gmail.com>
2023-09-08 09:41:10 +0100

    optimize landing page images

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import image1 from "../assets/landing1.svg";
 import image2 from "../assets/loan2.png";
 import image3 from "../assets/loan6.png";
-import image4 from "../assets/finance.svg"
-import image5 from "../assets/loan7.svg"
+import image4 from "../assets/finance.svg";
+import image5 from "../assets/loan7.svg";
 
 const Slider = () => {
-  const images = [image1, image2, image3,image4, image5];
-   const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [image1, image2, image3, image4, image5];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const next = (currentIndex + 1) % images.length;
@@ -17,12 +17,13 @@ const Slider = () => {
   return (
     <div className="carousel">
       {images.map((img, index) => (
-        <div
+        <img
           key={index}
+          loading="lazy"
+          src={img}
           className={`carousel__image ${
             index === currentIndex ? "active" : ""
           }`}
-          style={{ backgroundImage: `url(${img})` }}
         />
       ))}
     </div>
