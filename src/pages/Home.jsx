1c7a5ac62d4888efce2093255163d0647c43731d
ochosteve <ochosteve08@gmail.com>
2023-09-08 12:03:42 +0100

    remove welcome text from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from "react-router-dom";
-import AddBudgetForm from "../components/AddBudgetForm"
+import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
 import BudgetItem from "../components/BudgetItem";
 import Table from "../components/Table";
-
 
 const Home = ({ username, budgets, expenses }) => {
   return (
     <div className="dashboard">
       <h2>
-        Welcome back, <span className="accent">{username}</span>
+        Welcome, <span className="accent">{username}</span>
       </h2>
 
       <div className="grid-sm">
@@ -29,14 +28,22 @@ const Home = ({ username, budgets, expenses }) => {
                   <BudgetItem key={budget.id} budget={budget} />
                 ))}
             </div>
-            {expenses && expenses.length > 0 && 
-            <div className="grid-md">
-              <h2>Recent Expenses</h2>
-              <Table expenses = {expenses.sort((a,b)=> b.createdAt - a.createdAt).slice(0,5)} />
-              { expenses.length > 5 && <Link to={'expenses'} className="btn btn--dark"
-              > View all expenses</Link> }
-            </div>
-            }
+            {expenses && expenses.length > 0 && (
+              <div className="grid-md">
+                <h2>Recent Expenses</h2>
+                <Table
+                  expenses={expenses
+                    .sort((a, b) => b.createdAt - a.createdAt)
+                    .slice(0, 5)}
+                />
+                {expenses.length > 5 && (
+                  <Link to={"expenses"} className="btn btn--dark">
+                    {" "}
+                    View all expenses
+                  </Link>
+                )}
+              </div>
+            )}
           </div>
         ) : (
           <div className="grid-sm">
@@ -49,4 +56,4 @@ const Home = ({ username, budgets, expenses }) => {
   );
 };
 
-export default Home
+export default Home;
